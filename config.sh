71d1f18dd156358b8e3dc1fbae7ff2dee721d50f
hinto.janaiyo <hinto.janaiyo@protonmail.com>
2022-08-07 20:39:42 -0400

    add git tag

--- config.sh
+++ config.sh
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#git <libconfig/config.sh/a108ebf>
+#git <libconfig/config.sh/4e98d1d>
 
 config() {
 	# init local variables
